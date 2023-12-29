473d2fa77bed73095fe2346ccdd1d41a7edef3e1
nimrod.wagner <nimrod.wagner@perception-point.io>
2023-12-29 04:38:54 +0200

    Removed MainBar menu icon

--- src/components/MainBar/MainBar.tsx
+++ src/components/MainBar/MainBar.tsx
@@ -1,22 +1,10 @@
 import MapIcon from "@mui/icons-material/Map";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
-import {
-  AppBar,
-  Container,
-  IconButton,
-  InputAdornment,
-  TextField,
-  Toolbar,
-  Typography
-} from "@mui/material";
+import { AppBar, Container, InputAdornment, TextField, Toolbar, Typography } from "@mui/material";
 import useSearchParams, { EGlobalSearchParams } from "src/hooks/useSearchParams";
 import { ERoute, useIsFullScreenRoute } from "src/routes";
-import { setIsSideMenuOpen } from "src/store/global";
-import { AppDispatch } from "src/store/store";
 
 import { useIntl } from "react-intl";
-import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import UserProfile from "../UserProfile/UserProfile";
@@ -26,7 +14,6 @@ import messages from "./messages";
 export default function MainBar() {
   const { formatMessage } = useIntl();
 
-  const dispatch: AppDispatch = useDispatch();
   const navigate = useNavigate();
   const isFullScreenPage = useIsFullScreenRoute();
   const { searchParams, handleSearchParamChange } = useSearchParams({
@@ -37,17 +24,6 @@ export default function MainBar() {
     <AppBar position="sticky">
       <Container className={style.container} maxWidth={isFullScreenPage ? false : "lg"}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => dispatch(setIsSideMenuOpen(true))}
-          >
-            <MenuIcon />
-          </IconButton>
-
           <Typography className={style.labelContainer} variant="h6">
             <div className={style.label} onClick={() => navigate(ERoute.Home)}>
               <MapIcon />
@@ -60,7 +36,6 @@ export default function MainBar() {
             value={searchParams.get(EGlobalSearchParams.Search)}
             name={EGlobalSearchParams.Search}
             onChange={handleSearchParamChange}
-            variant="outlined"
             classes={{ root: style.searchBar }}
             InputProps={{
               startAdornment: (
